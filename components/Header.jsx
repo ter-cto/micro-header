45e9712018d7c20802d89559472e5d89028c0b88
Nithipong Jamdonprai <nithipong.ja@central.tech>
2020-12-03 00:00:57 +0700

    init

---
+++ components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => (
+  <div
+    style={{
+      backgroundColor: "#f5f5f5",
+      width: "100%",
+      padding: "15px",
+    }}
+  >
+    Header
+  </div>
+);
+
+export default Header;
